b7ccd867681112c555e1a5f1f728306e52e5b371
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-04-26 01:51:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+
+function theBeatlesPlay(arrmusic, arrinstru) {
+  var arr = [];
+  
+  for (var i = 0; i < arr.length; i++){
+    arr.push(arrmusic[0] + " plays " + arrinstru[0])
+  }
+  
+  return arr
+}
